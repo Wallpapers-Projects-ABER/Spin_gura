c9b27ece2435e965a4efef78fa82187ce4353262
ABER1047 <78880883+ABER1047@users.noreply.github.com>
2022-12-05 00:42:26 +0900

    2022_12_05
    added new skin
    fixed minor bugs
    added tutorial animation
    optimizing
    etc.

--- scripts.js
+++ scripts.js
@@ -1,3 +1,19 @@
+////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////
+/////change this value to "1" or "0" if you want to change skin/////
+////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////
+
+
+
+var skin_type = 0;
+
+
+
+////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////
 var mousepressed = 0;
 var b_mb_pos_x = -4;
 var b_mb_pos_y = -4;
@@ -14,105 +30,168 @@ var fps = 10;
 var air_resistence = 0.67;
 
 var do_not_show_guide = 0;
+var view_width = window.innerWidth;
+var c_w = view_width/1920;
+var real_skin_type = -4;
+var setting_now = 0;
+
+
+//instance
+var ins_air_res_bar = document.querySelector(".air_res_bar");
+var ins_air_res = document.querySelector(".air_res");
+var ins_type_change = document.querySelector(".type_change");
+var ins_obj_guide = document.querySelector(".obj_guide");
+var ins_obj_gura = document.querySelector(".obj_gura");
+var ins_obj_guide_cursor = document.querySelector(".obj_guide_cursor");
+var ins_guide_text = document.querySelector(".guide_text");
+var ins_guide_line = document.querySelector(".guide_line");
 
 
 
-//air resistence button
-document.querySelector(".air_res_bar").addEventListener("input",function()
-{
-air_resistence = (500-document.getElementById("air_res_bar").value)/500;
-console.log("air_resistence"+air_resistence);
-})
 
 
 
+
+
+//setting for first
 window.onload = function()
 {
-var view_width = window.innerWidth;
-document.querySelector(".obj_gura").style.left = (view_width/2-140)+"px";
-document.querySelector(".obj_gura").style.opacity = 1;
+ins_obj_gura.style.left = (view_width/2-140)+"px";
+ins_obj_gura.style.opacity = 1;
 
-document.querySelector(".air_res").style.opacity = 1;
-document.querySelector(".air_res_bar").style.opacity = 1;
+
+skin_change_apply()
+
+ins_air_res.style.opacity = 1;
+ins_air_res_bar.style.opacity = 1;
+ins_type_change.style.opacity = 1;
 // real_n_mb_pos_x = irandom_range(0,1980);
 // b_mb_pos_x = irandom_range(0,1980);
-document.querySelector(".obj_guide").style.left = (view_width/2+40)+"px";
-
-
+ins_obj_guide.style.left = (view_width/2+40)+"px";
 guide_mes();
 }
 
 window.addEventListener("resize", function()
 {
-var view_width = window.innerWidth;
-document.querySelector(".obj_gura").style.left = (view_width/2-140)+"px";
-document.querySelector(".obj_gura").style.opacity = 1;
+view_width = window.innerWidth;
+ins_obj_gura.style.left = (view_width/2-140)+"px";
+ins_obj_gura.style.opacity = 1;
 
-document.querySelector(".air_res").style.opacity = 1;
-document.querySelector(".air_res_bar").style.opacity = 1;
+ins_air_res.style.opacity = 1;
+ins_air_res_bar.style.opacity = 1;
 
-document.querySelector(".obj_guide").style.left = (view_width/2+40)+"px";
+ins_obj_guide.style.left = (view_width/2+40)+"px";
 })
 
 
 
+
+
+
+//apply changed skin
+function skin_change_apply()
+{
+    if (skin_type == 0)
+    {
+    real_skin_type = "";
+    }
+    else
+    {
+    real_skin_type = "B_";
+    }
+ins_obj_gura.style.src = "source/imgs/"+real_skin_type+n_img_3d_angle+".png";
+}
+
+
+
+
+
+//air resistence button
+ins_air_res_bar.addEventListener("input",function()
+{
+air_resistence = (500-document.getElementById("air_res_bar").value)/500;
+console.log("air_resistence"+air_resistence);
+})
+
+ins_air_res_bar.addEventListener("mouseover",function()
+{
+ins_air_res_bar.style.backgroundColor = "#bdb7bb";
+setting_now = 1;
+})
+
+ins_air_res_bar.addEventListener("mouseleave",function()
+{
+ins_air_res_bar.style.backgroundColor = "#dad4d8";
+setting_now = 0;
+})
+
+
+
+
+
+//guide animations 1
 function guide_cursor_anime1()
 {
-document.querySelector(".obj_guide_cursor").style.opacity = 0.8;
+ins_obj_guide_cursor.style.opacity = 0.8;
 setTimeout(guide_cursor_anime2,1000);
 }
 
 function guide_cursor_anime2()
 {
-var view_width = window.innerWidth;
-document.querySelector(".obj_guide_cursor").src = "imgs/icon4.png";
-document.querySelector(".obj_guide_cursor").style.width = 70+"px";
-document.querySelector(".obj_guide_cursor").style.left = (view_width/2+200)+"px";
+ins_obj_guide_cursor.src = "source/imgs/icon4.png";
+ins_obj_guide_cursor.style.width = 70+"px";
+ins_obj_guide_cursor.style.left = (view_width/2+200)+"px";
 
-document.querySelector(".guide_text").style.opacity = 0.8;
-document.querySelector(".guide_text").style.left = (view_width/2+150)+"px";
+ins_guide_text.style.opacity = 0.8;
+ins_guide_text.style.left = (view_width/2+150)+"px";
+
+ins_guide_line.style.opacity = 0.8;
+ins_guide_line.style.width = 215+"px";
 setTimeout(guide_cursor_anime3,1500);
 }
 
 function guide_cursor_anime3()
 {
-document.querySelector(".obj_guide_cursor").src = "imgs/icon3.png";
-document.querySelector(".obj_guide_cursor").style.width = 80+"px";
-document.querySelector(".obj_guide_cursor").style.opacity = 0;
+ins_obj_guide_cursor.src = "source/imgs/icon3.png";
+ins_obj_guide_cursor.style.width = 80+"px";
+ins_obj_guide_cursor.style.opacity = 0;
 
-document.querySelector(".guide_text").style.opacity = 0;
+ins_guide_text.style.opacity = 0;
+
+ins_guide_line.style.opacity = 0;
 setTimeout(guide_cursor_anime4,1000);
 }
 
 function guide_cursor_anime4()
 {
-var view_width = window.innerWidth;
-document.querySelector(".obj_guide_cursor").style.left = (view_width/2-40)+"px";
-document.querySelector(".guide_text").style.left = 54+"%";
+ins_obj_guide_cursor.style.left = (view_width/2-40)+"px";
+ins_guide_text.style.left = 54+"%";
     if (do_not_show_guide == 0)
     {
-    document.querySelector(".obj_guide_cursor").style.opacity = 0;
-    document.querySelector(".guide_text").style.opacity = 0;
+    ins_obj_guide_cursor.style.opacity = 0;
+    ins_guide_text.style.opacity = 0;
+    ins_guide_line.style.width = 0+"px";
     setTimeout(guide_cursor_anime1,1000);
     }
 }
 
 
-
+//default setting
 function guide_mes()
 {
-document.querySelector(".obj_guide").style.opacity = 1;
-document.querySelector(".obj_guide").style.width = 70+"px";
+ins_obj_guide.style.opacity = 1;
+ins_obj_guide.style.width = 70+"px";
 setTimeout(guide_mes_anime1,500);
 }
 
 
+//guide animations 2
 function guide_mes_anime1()
 {
     if (do_not_show_guide == 0)
     {
-    document.querySelector(".obj_guide").style.width = 60+"px";
-    document.querySelector(".obj_guide").style.transition = "width 0.4s";
+    ins_obj_guide.style.width = 60+"px";
+    ins_obj_guide.style.transition = "width 0.4s";
     setTimeout(guide_mes_anime2,270);
     }
 }
@@ -121,7 +200,7 @@ function guide_mes_anime2()
 {
     if (do_not_show_guide == 0)
     {
-    document.querySelector(".obj_guide").style.width = 66+"px";
+    ins_obj_guide.style.width = 66+"px";
     setTimeout(guide_mes_anime3,270);
     }
 }
@@ -130,7 +209,7 @@ function guide_mes_anime3()
 {
     if (do_not_show_guide == 0)
     {
-    document.querySelector(".obj_guide").style.width = 62+"px";
+    ins_obj_guide.style.width = 62+"px";
     setTimeout(guide_mes_anime4,270);
     }
 }
@@ -139,20 +218,46 @@ function guide_mes_anime4()
 {
     if (do_not_show_guide == 0)
     {
-    document.querySelector(".obj_guide").style.width = 64+"px";
+    ins_obj_guide.style.width = 64+"px";
     }
 }
 
-
-//guide opened
-document.querySelector(".obj_guide").addEventListener("click",function()
+function guide_mes_des_anime()
 {
-var view_width = window.innerWidth;
-document.querySelector(".obj_guide").style.width = 0+"px";
-document.querySelector(".obj_guide").style.opacity = 0;
+ins_obj_guide.style.top = 0;
+ins_obj_guide_cursor.style.top = 0;
+ins_guide_text.style.top = 0;
+ins_guide_line.style.top = 0;
+}
 
-document.querySelector(".guide_text").style.left = 54+"%";
-document.querySelector(".obj_guide_cursor").style.left = (view_width/2-40)+"px";
+
+
+
+
+
+
+
+
+//guide button
+ins_obj_guide.addEventListener("mouseover",function()
+{
+setting_now = 1;
+})
+
+ins_obj_guide.addEventListener("mouseleave",function()
+{
+setting_now = 0;
+})
+
+ins_obj_guide.addEventListener("click",function()
+{
+ins_obj_guide.style.width = 0+"px";
+ins_obj_guide.style.opacity = 0;
+
+ins_guide_text.style.left = 54+"%";
+ins_obj_guide_cursor.style.left = (view_width/2-40)+"px";
+
+ins_guide_line.style.left = (view_width/2+10)+"px";
 guide_cursor_anime1();
 })
 
@@ -160,33 +265,68 @@ guide_cursor_anime1();
 
 
 
-// pressing now
+
+
+
+//skin change button
+ins_type_change.addEventListener("mouseover",function()
+{
+ins_type_change.style.backgroundColor = "white";
+setting_now = 1;
+})
+
+ins_type_change.addEventListener("mouseleave",function()
+{
+ins_type_change.style.backgroundColor = "#ffffff00";
+setting_now = 0;
+})
+
+ins_type_change.addEventListener("click",function()
+{
+skin_type ++;
+    if (skin_type > 1)
+    {
+    skin_type = 0;
+    }
+skin_change_apply();
+})
+
+
+
+
+//pressing now (global mouse button)
 document.addEventListener("mousedown",function()
 {
-mousepressed = 1;
-b_mb_pos_x = event.clientX;
-b_mb_pos_y = event.clientY;
-real_n_mb_pos_x = event.clientX;
-real_n_mb_pos_y = event.clientY;
+    if (setting_now == 0)
+    {
+    mousepressed = 1;
+    b_mb_pos_x = event.clientX;
+    b_mb_pos_y = event.clientY;
+    real_n_mb_pos_x = event.clientX;
+    real_n_mb_pos_y = event.clientY;
 
-console.log("B("+b_mb_pos_x+","+b_mb_pos_y+")");
+    console.log("B("+b_mb_pos_x+","+b_mb_pos_y+")");
+    }
 })
 
-// released
+//released (global mouse button)
 document.addEventListener("mouseup",function()
 {
-mousepressed = 0;
-pertential_angle = event.clientX;
-pertential_angle_sec = event.clientX;
-//n_mb_pos_x = event.clientX;
-//n_mb_pos_y = event.clientY;
-console.log("N("+real_n_mb_pos_x+","+real_n_mb_pos_y+")");
+    if (setting_now == 0)
+    {
+    mousepressed = 0;
+    pertential_angle = event.clientX;
+    pertential_angle_sec = event.clientX;
+    //n_mb_pos_x = event.clientX;
+    //n_mb_pos_y = event.clientY;
+    console.log("N("+real_n_mb_pos_x+","+real_n_mb_pos_y+")");
+    }
 })
 
-
+//dragging (global mouse button)
 document.addEventListener("mousemove",function()
 {
-    if (mousepressed == 1)
+    if (setting_now == 0 && mousepressed == 1)
     {
     real_n_mb_pos_x = event.clientX;
     real_n_mb_pos_y = event.clientY;
@@ -210,7 +350,7 @@ document.addEventListener("mousemove",function()
         n_img_3d_angle = 1+(n_img_3d_angle-22);
         }
 
-    document.querySelector(".obj_gura").src = "imgs/"+round(n_img_3d_angle)+".png";
+    ins_obj_gura.src = "source/imgs/"+real_skin_type+round(n_img_3d_angle)+".png";
     }
 })
 
@@ -222,30 +362,35 @@ document.addEventListener("mousemove",function()
 setTimeout(step_event,fps);
 function step_event() //10 fps
 {
+    //calculate resistance (friction)
     if ((b_mb_pos_x - real_n_mb_pos_x) != 0)
     {
-    pertential_angle_sec += (real_n_mb_pos_x - pertential_angle_sec)*(air_resistence/25);
-    pertential_angle += (pertential_angle_sec - pertential_angle)*(air_resistence/25);
-    b_mb_pos_x += (pertential_angle - b_mb_pos_x)*(air_resistence/25);
+    pertential_angle_sec += (real_n_mb_pos_x - pertential_angle_sec)*(air_resistence/75);
+    pertential_angle += (pertential_angle_sec - pertential_angle)*(air_resistence/75);
+    b_mb_pos_x += (pertential_angle - b_mb_pos_x)*(air_resistence/75);
     
-    do_not_show_guide = 1;
+        if (do_not_show_guide != 1)
+        {
+        do_not_show_guide = 1;
 
-    document.querySelector(".obj_guide").style.width = 0+"px";
-    document.querySelector(".obj_guide").style.opacity = 0;
+        ins_obj_guide.style.width = 0+"px";
+        ins_obj_guide.style.opacity = 0;
 
-    document.querySelector(".obj_guide_cursor").style.transition = "opacity 0s";
-    document.querySelector(".obj_guide_cursor").style.opacity = 0;
+        ins_obj_guide_cursor.style.opacity = 0;
 
-    document.querySelector(".guide_text").style.transition = "opacity 0s";
-    document.querySelector(".guide_text").style.opacity = 0;
+        ins_guide_text.style.opacity = 0;
+        setTimeout(guide_mes_des_anime,2000);
+        }
     }
 
 
-
+//apply resistance (friction)
 var distance_from_b = (b_mb_pos_x - real_n_mb_pos_x)/640;
 n_img_3d_angle += distance_from_b;
 //console.log("distance_from_b (potential) : "+distance_from_b);
 
+
+//apply image
     if (abs(distance_from_b) > 7)
     {
     distance_from_b = 7*sign(distance_from_b)
@@ -261,8 +406,20 @@ n_img_3d_angle += distance_from_b;
     n_img_3d_angle = 1+(n_img_3d_angle-22);
     }
     
-document.querySelector(".obj_gura").src = "imgs/"+round(n_img_3d_angle)+".png";
+ins_obj_gura.src = "source/imgs/"+real_skin_type+round(n_img_3d_angle)+".png";
 
 setTimeout(step_event,fps);
 }
 
+
+
+
+
+
+
+
+
+//set values for css
+var resizing0 = 14*c_w;
+document.documentElement.style.setProperty("--text_size",resizing0+"px");
+document.documentElement.style.setProperty("--skin_change_pos",view_width-resizing0*5+"px");
