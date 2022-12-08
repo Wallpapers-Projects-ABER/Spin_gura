d0ec5b4c688c31eae80a4b25864649458b7cdc2f
ABER1047 <78880883+ABER1047@users.noreply.github.com>
2022-12-09 00:43:18 +0900

    test

--- scripts.js
+++ scripts.js
@@ -40,7 +40,7 @@ var setting_now = 0;
 var loading_now = 1;
 var loading_time = 8000;
 
-var check_imgs_loaded = 1
+var check_imgs_loaded = 0;
 
 
 //instance
@@ -59,10 +59,40 @@ var ins_resistence_value_text = document.querySelector(".resistence_value_text")
 
 
 
+//checking images are loaded
+$(document).ready(function() 
+{
+// Images loaded is zero because we're going to process a new set of images.
+var imagesLoaded = 0;
+// Total images is still the total number of <img> elements on the page.
+var totalImages = $("img").length;
+
+    // Step through each image in the DOM, clone it, attach an onload event
+    // listener, then set its source to the source of the original image. When
+    // that new image has loaded, fire the imageLoaded() callback.
+    $("img").each(function (idx, img) 
+    {
+    $("<img>").on("load", imageLoaded).attr("src", $(img).attr("src"));
+    });
+
+    // Do exactly as we had before -- increment the loaded count and if all are
+    // loaded, call the allImagesLoaded() function.
+    function imageLoaded()
+    {
+    imagesLoaded++;
+        if (imagesLoaded == totalImages) 
+        {
+        check_imgs_loaded = 1;
+        }
+    }
+});
 
 
-
-
+function loading_imgs()
+{
+loading_time -= 7000*check_imgs_loaded;
+setTimeout(loading_anime,loading_time);
+}
 
 
 //setting for first
@@ -71,27 +101,13 @@ window.onload = function()
 ins_obj_gura.style.left = (view_width/2-140)+"px";
 ins_obj_gura.style.opacity = 1;
 
-// When we begin, assume no images are loaded.
-var images_loaded = 0;
-// Count the total number of images on the page when the page has loaded.
-var total_images = document.querySelector("img").length;
-
-// After an image is loaded, add to the count, and if that count equals the
-// total number of images, fire the allImagesLoaded() function.
-document.querySelector("img").addEventListener("load", function() 
-{
-images_loaded++;
-    if (images_loaded == total_images) 
-    {
-    check_imgs_loaded = 1;
-    }
-});
+ins_loading_gif.style.opacity = 1;
+ins_loading_gif.style.transition = "opacity 0.2s";
 
 
 //loading
-loading_time -= 7000*check_imgs_loaded;
 console.log("loading_time");
-setTimeout(loading_anime,loading_time);
+setTimeout(loading_imgs,1000)
 skin_change_apply()
 
 ins_air_res.style.opacity = 1;
